0bf92151713f68ed449fbd79dec615da1d592073
PKL (GLA) <friscafebrianti16@gmail.com>
2023-10-19 15:58:21 +0700

    keyboard samples

---
+++ app/src/main/java/com/example/keyboardsamples/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.keyboardsamples;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView;
+
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
+
+    private String mSpinnerLabel = "";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the spinner.
+        Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+    }
+
+    private void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        if (editText != null) {
+            // Assign to showString both the entered string and mSpinnerLabel.
+            String showString = (editText.getText().toString() + " - " + mSpinnerLabel);
+            // Assign to phoneNumberResult the view for text_phonelabel to prepare to show it.
+            TextView phoneNumberResult = (TextView) findViewById(R.id.text_phonelabel);
+            // Show the showString in the phoneNumberResult.
+            if (phoneNumberResult != null) phoneNumberResult.setText(showString);
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        mSpinnerLabel = adapterView.getItemAtPosition(i).toString();
+        showText(view);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, getString(R.string.nothing_selected));
+
+    }
+}
